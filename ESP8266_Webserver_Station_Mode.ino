4a4338f4d4a51239a6f21b8f27fe14cf38f7fe0b
BINARYUPDATES.COM <binaryupdates@gmail.com>
2022-02-02 19:39:23 +0530

    Update ESP8266_Webserver_Station_Mode.ino

--- ESP8266_Webserver_Station_Mode.ino
+++ ESP8266_Webserver_Station_Mode.ino
@@ -7,14 +7,12 @@ const char* password = "bestcourses"; // Enter Password here
 
 ESP8266WebServer server(80);
 
-bool D1Pinstatus = LOW;
-bool D2Pinstatus = LOW;
+bool LEDstatus = LOW;
 
 void setup() {
   Serial.begin(9600);
   delay(100);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
+  pinMode(D4, OUTPUT);
 
   Serial.println("Connecting to ");
   Serial.println(ssid);
@@ -29,13 +27,12 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+  Serial.print("Got IP: ");  
+  Serial.println(WiFi.localIP());
 
   server.on("/", handle_OnConnect);
-  server.on("/d1on", handle_d1on);
-  server.on("/d1off", handle_d1off);
-  server.on("/d2on", handle_d2on);
-  server.on("/d2off", handle_d2off);
+  server.on("/ledon", handle_ledon);
+  server.on("/ledoff", handle_ledoff);
   server.onNotFound(handle_NotFound);
 
   server.begin();
@@ -43,57 +40,42 @@ void setup() {
 }
 void loop() {
   server.handleClient();
-  if(D1Pinstatus)
-  {digitalWrite(D1, HIGH);}
-  else
-  {digitalWrite(D1, LOW);}
   
-  if(D2Pinstatus)
-  {digitalWrite(D2, HIGH);}
+  if(LEDstatus)
+  {
+    digitalWrite(D4, HIGH);}
   else
-  {digitalWrite(D2, LOW);}
+  {
+    digitalWrite(D4, LOW);}
 }
 
 void handle_OnConnect() {
-  D1Pinstatus = LOW;
-  D2Pinstatus = LOW;
-  Serial.println("LED D1: OFF | LED D2: OFF");
-  server.send(200, "text/html", updateWebpage(D1Pinstatus,D2Pinstatus)); 
+  LEDstatus = LOW;
+  Serial.println("LED: OFF");
+  server.send(200, "text/html", updateWebpage(LEDstatus)); 
 }
 
-void handle_d1on() {
-  D1Pinstatus = HIGH;
-  Serial.println("LED D1: ON");
-  server.send(200, "text/html", updateWebpage(true,D2Pinstatus)); 
+void handle_ledon() {
+  LEDstatus = HIGH;
+  Serial.println("LED: ON");
+  server.send(200, "text/html", updateWebpage(LEDstatus)); 
 }
 
-void handle_d1off() {
-  D1Pinstatus = LOW;
-  Serial.println("LED D1: OFF");
-  server.send(200, "text/html", updateWebpage(false,D2Pinstatus)); 
-}
-
-void handle_d2on() {
-  D2Pinstatus = HIGH;
-  Serial.println("LED D2: ON");
-  server.send(200, "text/html", updateWebpage(D1Pinstatus,true)); 
-}
-
-void handle_d2off() {
-  D2Pinstatus = LOW;
-  Serial.println("LED D2: OFF");
-  server.send(200, "text/html", updateWebpage(D1Pinstatus,false)); 
+void handle_ledoff() {
+  LEDstatus = LOW;
+  Serial.println("LED: OFF");
+  server.send(200, "text/html", updateWebpage(LEDstatus)); 
 }
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
-String updateWebpage(uint8_t d1Pinstate,uint8_t d2Pinstate){
+String updateWebpage(uint8_t LEDstatus){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   ptr +="<title>LED Control</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="<style>html {font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
   ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
   ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
   ptr +=".button-on {background-color: #3498db;}\n";
@@ -105,20 +87,13 @@ String updateWebpage(uint8_t d1Pinstate,uint8_t d2Pinstate){
   ptr +="</head>\n";
   ptr +="<body>\n";
   ptr +="<h1>ESP8266 Web Server</h1>\n";
-    ptr +="<h3>Using Station(STA) Mode</h3>\n";
+  ptr +="<h3>Using Station(STA) Mode</h3>\n";
   
-   if(d1Pinstate){
-    ptr +="<p>GREEN LED: ON</p><a class=\"button button-off\" href=\"/d1off\">OFF</a>\n";
+   if(LEDstatus){
+    ptr +="<p>BLUE LED: ON</p><a class=\"button button-off\" href=\"/ledoff\">OFF</a>\n";
    }else{
-    ptr +="<p>GREEN LED: OFF</p><a class=\"button button-on\" href=\"/d1on\">ON</a>\n";
+    ptr +="<p>BLUE LED: OFF</p><a class=\"button button-on\" href=\"/ledon\">ON</a>\n";
    }
-
-  if(d2Pinstate){
-    ptr +="<p>RED LED: ON</p><a class=\"button button-off\" href=\"/d2off\">OFF</a>\n";
-  }
-  else{
-    ptr +="<p>RED LED: OFF</p><a class=\"button button-on\" href=\"/d2on\">ON</a>\n";
-  }
 
   ptr +="</body>\n";
   ptr +="</html>\n";
